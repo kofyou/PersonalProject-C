5be2d5e3d1412bdf43196ee85f77c73ec30b24a1
fujiangfer <1632816175@qq.com>
2021-02-28 20:15:13 +0800

    wordcount5.0
    改进排序速度，提高性能；添加了文件打开失败的提示，因为含空白符空行和一些不知道的错误修改了字符统计和行数统计的函数。

--- 221801131/example/src/Main.cpp
+++ 221801131/example/src/Main.cpp
@@ -4,11 +4,19 @@
 int main(int argv, char** argc) {
     char* input = argc[1];
     char* output = argc[2];
+    if (!input)
+    {
+        cout << "未输入文件名或文件不存在" << endl;
+        return 0;
+    }
     WordCount* test = new WordCount(argc[1]);
     ofstream outfile(output, ios::out);
-    outfile << "characters:" << test->getcharacternum() << endl;      //字符数
+    if (!outfile) {
+        cout << "文件打开失败！" << endl;
+    }
+    outfile << "characters:" << test->getcharacternum() << endl;
     outfile << "words:" << test->getwordnum1() << endl;               //单词数
-    outfile << "lines:" << test->getlinenum() << endl;                //行数
+    outfile << "lines:" << test->getlinenum() << endl;
     Words* wwords = test->getwords();
     for (int i = 0; i < test->getwordnum2(); i++) {
         if (i == 10)break;
