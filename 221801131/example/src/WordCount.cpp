5e8145ce01b988af04e64b5b6d1f9c725c2ac4b4
fujiangfer <1632816175@qq.com>
2021-02-28 11:28:17 +0800

    wordcount2.0
    实现了单词数计算

--- 221801131/example/src/WordCount.cpp
+++ 221801131/example/src/WordCount.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int charactersCount(char* Path);
+int wordCount(char* Path);
 
 int main(int argv, char** argc) {
     char* input = argc[1];
@@ -11,6 +12,8 @@ int main(int argv, char** argc) {
     count = charactersCount(input);
     ofstream outfile(output, ios::out);
     outfile << "characters:" << count << endl;
+    count = wordCount(input);
+    outfile << "words:" << count << endl;               //单词数
 
 
     outfile.close();
@@ -22,6 +25,37 @@ int charactersCount(char* Path) {    //计算字符数量
     int num = 0;
     while ((code = infile.get()) != EOF) {
         if (code >= 0 && code <= 127)num++;
+    }
+    infile.close();
+    return num;
+}
+
+int wordCount(char* Path) {    //计算单词个数
+    ifstream infile(Path);
+    char word;
+    int charCount = 0;//记录字母数
+    int num = 0;
+    bool flag = false;
+    while ((word = infile.get()) != EOF) {
+        if (flag)
+        {
+            if (!((word >= 'a' && word <= 'z') || (word >= 'A' && word <= 'Z') || (word >= '0' && word <= '9'))) { //确认一个单词后寻找下一个间隔符
+                flag = false;
+                charCount = 0;
+            }
+        }
+        else {
+            if ((word >= 'a' && word <= 'z') || (word >= 'A' && word <= 'Z')) {    //判断是否是字母
+                charCount++;
+                if (charCount == 4) {     //4个英文字母开头时计一个单词数并寻找下一个间隔符
+                    num++;
+                    flag = true;
+                }
+            }
+            else {                    //没有4个英文字母开头则寻找下一个间隔符
+                flag = true;
+            }
+        }
     }
     infile.close();
     return num;
