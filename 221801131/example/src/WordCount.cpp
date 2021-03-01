39c0d53ccad475d2aa107b7a7783e0499fcca52f
fujiangfer <1632816175@qq.com>
2021-03-01 19:47:29 +0800

    编程风格写入，更正一些问题
    vscode搞格式化会把{单独弄一行，vs2019不会，写的时候没单独一行，出现了一些不一致。
    修复了改进排序后忘记频次相同时按字典排序的问题。

--- 221801131/example/src/WordCount.cpp
+++ 221801131/example/src/WordCount.cpp
@@ -1,21 +1,27 @@
-#include "WordCount.h"
+#include"WordCount.h"
 using namespace std;
-typedef pair<string, int> PAIR;
-bool cmp_by_value(const PAIR &lhs, const PAIR &rhs)
-{
-    return lhs.second > rhs.second;
-}
 
-struct CmpByValue
-{
-    bool operator()(const PAIR &lhs, const PAIR &rhs)
-    {
-        return lhs.second > rhs.second;
+typedef pair<string, int> PAIR;
+
+struct CmpByValue {
+    bool operator()(const PAIR& lhs, const PAIR& rhs) {
+        if (lhs.second == rhs.second) {//词频相同时
+            if (lhs.first.compare(rhs.first) > 0)return false;
+            else return true;
+        }
+        else return lhs.second > rhs.second;
     }
 };
 
-WordCount::WordCount(char *Path)
-{
+WordCount::WordCount() {
+    wordnum2 = 0;
+    characternum = 0;
+    wordnum1 = 0;
+    linenum = 0;
+    wwords = new Words[wordnum2];
+}
+
+WordCount::WordCount(char* Path) {
     wordnum2 = 0;
     characternum = charactersCount(Path);
     wordnum1 = wordCount(Path);
@@ -24,36 +30,27 @@ WordCount::WordCount(char *Path)
     wordsort();
 }
 
-int WordCount::getcharacternum()
-{
+int WordCount::getcharacternum() {
     return characternum;
 }
-
-int WordCount::getlinenum()
-{
+int WordCount::getlinenum() {
     return linenum;
 }
-
-int WordCount::getwordnum1()
-{
+int WordCount::getwordnum1() {
     return wordnum1;
 }
 
-int WordCount::getwordnum2()
-{
+int WordCount::getwordnum2() {
     return wordnum2;
 }
 
-Words *WordCount::getwords()
-{
+Words* WordCount::getwords() {
     return wwords;
 }
 
-int WordCount::charactersCount(char *Path)
-{ //计算字符数量
+int WordCount::charactersCount(char* Path) {    //计算字符数量
     ifstream infile(Path);
-    if (!infile)
-    {
+    if (!infile) {
         cout << "文件打开失败！" << endl;
     }
     char code;
@@ -65,43 +62,37 @@ int WordCount::charactersCount(char *Path)
     while (getline(infile, str)) //以行为单位读入文件
         num += str.size(); //累计字符数
      */
-    infile >> noskipws; //强制读入空格和换行符
+    infile >> noskipws;//强制读入空格和换行符
     while (!infile.eof())
     {
         infile >> code;
         if (infile.eof())
-            break; //防止最后一个字符输出两次
+            break;//防止最后一个字符输出两次
         num++;
     }
     infile.close();
     return num;
 }
 
-int WordCount::wordCount(char *Path)
-{ //计算单词个数
+int WordCount::wordCount(char* Path) {    //计算单词个数
     ifstream infile(Path);
-    if (!infile)
-    {
+    if (!infile) {
         cout << "文件打开失败！" << endl;
     }
     char word;
-    char *str = new char[100]; //记录单词
+    char* str = new char[100];  //记录单词
     bool flag = false;
     bool isWords = false;
-    int charCount = 0; //记录字母数
+    int charCount = 0;//记录字母数
     int num = 0;
     word = infile.get();
-    while (true)
-    {
-        if (word >= 'A' && word <= 'Z')
-            word += 32;
+    while (true) {
+        if (word >= 'A' && word <= 'Z')word += 32;
         if (flag)
         {
-            if (!((word >= 'a' && word <= 'z') || (word >= '0' && word <= '9')))
-            { //确认一个单词后寻找下一个间隔符
+            if (!((word >= 'a' && word <= 'z') || (word >= '0' && word <= '9'))) { //确认一个单词后寻找下一个间隔符
                 //cout << str << endl;
-                if (isWords)
-                {
+                if (isWords) {
                     str[charCount] = NULL;
                     safeWord(str);
                 }
@@ -110,34 +101,27 @@ int WordCount::wordCount(char *Path)
                 isWords = false;
                 charCount = 0;
             }
-            else if (isWords)
-            {
+            else if (isWords) {
                 str[charCount++] = word;
             }
         }
-        else
-        {
-            if (word >= 'a' && word <= 'z')
-            { //判断是否是字母
+        else {
+            if (word >= 'a' && word <= 'z') {    //判断是否是字母
                 str[charCount++] = word;
-                if (charCount == 4)
-                { //4个英文字母开头时计一个单词数并寻找下一个间隔符
+                if (charCount == 4) {     //4个英文字母开头时计一个单词数并寻找下一个间隔符
                     num++;
                     flag = true;
                     isWords = true;
                 }
             }
-            else
-            { //没有4个英文字母开头则寻找下一个间隔符
+            else {                    //没有4个英文字母开头则寻找下一个间隔符
                 flag = true;
                 continue;
             }
         }
-        if ((word = infile.get()) == EOF)
-            break;
+        if ((word = infile.get()) == EOF)break;
     }
-    if (isWords)
-    { //若最后一个为单词则保存
+    if (isWords) {                   //若最后一个为单词则保存
         str[charCount] = NULL;
         safeWord(str);
     }
@@ -145,25 +129,20 @@ int WordCount::wordCount(char *Path)
     return num;
 }
 
-int WordCount::lineCount(char *Path)
-{ //计算行
+int WordCount::lineCount(char* Path) {    //计算行
     ifstream infile(Path);
-    if (!infile)
-    {
+    if (!infile) {
         cout << "文件打开失败！" << endl;
     }
     string line;
     int num = 0;
-    while (!infile.eof())
-    {
+    while (!infile.eof()) {
         getline(infile, line);
         int i = 0;
-        while (i < line.length() && isspace(line[i]))
-        {
+        while (i < line.length() && isspace(line[i])){
             i++;
         }
-        if (i != line.length())
-        {
+        if (i != line.length()){
             num++;
         }
     }
@@ -171,30 +150,25 @@ int WordCount::lineCount(char *Path)
     return num;
 }
 
-void WordCount::safeWord(char *str)
-{
+void WordCount::safeWord(char* str) {
     string word;
     word = str;
-    map<string, int>::iterator iter = words.find(word); //找单词
-    if (iter == words.end())
-    {
-        words.insert(pair<string, int>(word, 1)); //没找到单词，插入单词并设定次数为1
-        wordnum2++;                               //单词个数+1
+    map<string, int>::iterator iter = words.find(word);//找单词 
+    if (iter == words.end()) {
+        words.insert(pair<string, int>(word, 1));//没找到单词，插入单词并设定次数为1 
+        wordnum2++;                              //单词个数+1
     }
-    else
-    {
-        iter->second++; //找到单词，单词出现次数增加
+    else {
+        iter->second++;//找到单词，单词出现次数增加 
     }
 }
 
-void WordCount::wordsort()
-{
-    Words temp;
+void WordCount::wordsort() {
+	Words temp;
     map<string, int>::iterator iter;
     iter = words.begin();
     int count = 0;
-    for (; iter != words.end(); iter++, count++)
-    {
+    for (; iter != words.end(); iter++, count++) {
         wwords[count].word = iter->first;
         wwords[count].count = iter->second;
     }
@@ -216,9 +190,8 @@ void WordCount::wordsort()
     }*/
     vector<PAIR> vwords(words.begin(), words.end());
     sort(vwords.begin(), vwords.end(), CmpByValue());
-    // sort(name_score_vec.begin(), name_score_vec.end(), cmp_by_value);
-    for (int i = 0; i != vwords.size(); ++i)
-    {
+    //sort(vwords.begin(), vwords.end(), cmp_by_value);
+    for (int i = 0; i != vwords.size(); ++i) {
         wwords[i].word = vwords[i].first;
         wwords[i].count = vwords[i].second;
     }
