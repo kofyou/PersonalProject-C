dccabc2766628e6ccace3d75713622c39f047a0f
Wuziian <zinianwu@qq.com>
2021-03-02 20:25:42 +0800

    完善词频排序输出

--- 221801228/src/WordCount.cpp
+++ 221801228/src/WordCount.cpp
@@ -1,9 +1,31 @@
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <algorithm>
 #include "CounterCore.h"
 
+void writeResult(const Core::CountResult& result, const std::string &file)
+{
+    std::ofstream os(file, std::ios_base::trunc);
+    if (!os.is_open())
+        throw std::exception(("can not write file " + file + "\n").c_str());
+    os << "characters: " << result.charCount << std::endl;
+    os << "words: " << result.wordCount << std::endl;
+    os << "lines: " << result.lineCount << std::endl;
 
+    using WordPair = std::pair<std::string, int>;
+    std::vector<WordPair> words(result.wordOccurs.begin(), result.wordOccurs.end());
+    std::stable_sort(words.begin(), words.end(), 
+        [](const WordPair& a, const WordPair& b)
+        {
+            return a.second > b.second;
+        }
+    );
+    for (int i = 0; i < 10 && i < words.size(); i++)
+        os << words[i].first << ": " << words[i].second << std::endl;
+        
+    os.close();
+}
 
 
 int main(int argc, char** argv)
@@ -11,16 +33,21 @@ int main(int argc, char** argv)
     using namespace std;
     if (argc != 3)
     {
-        cout << "usage: WordCount <inputfile> <outputfile>\n";
+        cout << "usage: " << argv[0] << " <inputfile> <outputfile>\n";
         exit(-1);
     }
 
     std::string inputFile(argv[1]);
     std::string outputFile(argv[2]);
 
-    auto result = Core::Counter::count(inputFile);
-    cout << "characters: " << result.charCount << endl;
-    cout << "words: " << result.wordCount << endl;
-    cout << "lines: " << result.lineCount << endl;
+    try
+    {
+        auto result = Core::Counter::count(inputFile);
+        writeResult(result, outputFile);
+    }
+    catch (exception e)
+    {
+        cout << e.what();
+    }
     return 0;
 }
