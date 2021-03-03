151781088c764c698f4f764698dcaa35ec1833f2
Flut12 <75604829+Flut12@users.noreply.github.com>
2021-03-03 22:45:22 +0800

    Update WordCount.cpp
    增加了不是用命令行参数输入的方法，并且修改了新的输出方式

--- 221801135/src/WordCount.cpp
+++ 221801135/src/WordCount.cpp
@@ -2,8 +2,21 @@
 
 int main(int argc, char* argv[])
 {
-	wordCount wordcount(argv[1], argv[2]);
-	wordcount.handleInPut();
-	wordcount.outPut();
+	if (argc == 3)
+	{
+		wordCount wordcount(argv[1], argv[2]);
+		wordcount.handleInPut();
+		wordcount.outPutAll();
+	}
+	else
+	{
+		string inFileName;
+		string outFileName;
+		cin >> inFileName >> outFileName;
+		wordCount wordcount(inFileName, outFileName);
+		wordcount.handleInPut();
+		wordcount.outPutAll();
+	}
+	
 	return 0;
 }
