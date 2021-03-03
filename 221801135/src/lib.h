766e3349d445f3f24a1beb331a9294cd5ce43ca4
Flut12 <75604829+Flut12@users.noreply.github.com>
2021-03-03 20:50:16 +0800

    Update lib.h
    完成类的封装，把文件操作改入类的构建和解构函数中

--- 221801135/src/lib.h
+++ 221801135/src/lib.h
@@ -9,22 +9,28 @@
 #include<map>
 #define MIN_WORD_SIZE 4
 using namespace std;
+class wordCount
+{
+public:
+	wordCount(string inFileName, string outFileName);
+	~wordCount();
+	void handleInPut();
+	void outPut();
 
-char singleCharacter;
-string singleWord = "";
-map<string, int> wordList;
-int characterCnt = 0;
-int wordCnt = 0;
-int cnt = 0;
-ifstream infile;
-ofstream outfile;
+	int handleCharacter(char c);//处理输入的每一个字符，用于 1.统计字符数量
+	void wordBreak();//当出现字母数字字符时进行单词的分割和判断
+	int newWord(string key, int value);//记录新的单词，用于 2.统计单词数量
+	bool isRepeatWord();//记录重复出现的单词词频，用于 3.统计最多的10个单词及其词频
 
-void inPut(string fileName);
-void outPut(string fileName);
-
-void handleCharacter(char c);//处理输入的每一个字符，用于 1.处理字符数量
-void wordBreak();//当出现字母数字字符时进行单词的分割和判断
-bool newWord(string key, int value);//记录新的单词，用于 2.统计单词数量
-bool isRepeatWord();//记录重复出现的单词词频，用于 3.统计最多的10个单词及其词频
+private:
+	char singleCharacter;
+	string singleWord = "";
+	map<string, int> wordList;
+	int characterCnt = 0;
+	int wordCnt = 0;
+	int cnt = 0;
+	ifstream infile;
+	ofstream outfile;
+};
 
 #endif
