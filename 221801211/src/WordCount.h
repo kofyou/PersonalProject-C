c560a6e9ad90a8258ef3d9dd9d0683fdc54335f0
gitSJF <79841415+gitSJF@users.noreply.github.com>
2021-03-03 13:54:56 +0800

    头文件宏定义的添加

--- 221801211/src/WordCount.h
+++ 221801211/src/WordCount.h
@@ -1,9 +1,11 @@
 #include<iostream>
 #include<string>
+#include<fstream>
 
 using namespace std;
 
 #define MAXWORD 20
+#define MAXWORDS 10000
 
 int GetCharacters(char* str);
 int GetWords(char* str);
