0bf7b12ece06e1cd4d78ada71337443b2e8265a2
gitSJF <79841415+gitSJF@users.noreply.github.com>
2021-03-04 08:26:07 +0800

    代码优化

--- 221801211/src/WordCount.cpp
+++ 221801211/src/WordCount.cpp
@@ -21,7 +21,14 @@ int GetWords(char* str)
 		{
 			if (j >= 4)
 			{
-				return 1;
+				if ((('a' <= temp[num][0] && temp[num][0] <= 'z') || ('A' <= temp[num][0] && temp[num][0] <= 'Z')) &&
+					(('a' <= temp[num][1] && temp[num][1] <= 'z') || ('A' <= temp[num][1] && temp[num][1] <= 'Z')) &&
+					(('a' <= temp[num][2] && temp[num][2] <= 'z') || ('A' <= temp[num][2] && temp[num][2] <= 'Z')) &&
+					(('a' <= temp[num][3] && temp[num][3] <= 'z') || ('A' <= temp[num][3] && temp[num][3] <= 'Z')))
+				{
+					return 1;
+				}
+				
 			}
 		}
 		else if (str[i] == ' '||i == strlen(str)-1||str[i]=='\n')
@@ -37,6 +44,10 @@ int GetWords(char* str)
 				}
 			}
 			j = 0;
+		}
+		if (num == 11)
+		{
+			break;
 		}
 	}
 	return num;
@@ -92,6 +103,10 @@ void Show(string str[], int n)
 				swap(str[i], str[j]);
 			}
 		}
+	}
+	if (n > 10)
+	{
+		n = 10;
 	}
 	for (int i = 0; i < n; i++)
 	{
