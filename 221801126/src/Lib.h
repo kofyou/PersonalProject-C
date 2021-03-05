939bbea65e3f570058276712e09a71255dba0bd8
yangzishen <76687060+yangzishen@users.noreply.github.com>
2021-03-05 16:24:47 +0800

    add some aid-fucs note
    添加函数声明 定义特定字符的ASCII码数值为常量

--- 221801126/src/Lib.h
+++ 221801126/src/Lib.h
@@ -1,5 +1,11 @@
 #pragma  once
 
+#define POINTER_DISTANCE_NUMBER 4 //pointer distance between 1to2 is four
+#define BLANK_SPACE_NUMBER 32 //blankspace
+#define LINE_FEED_NUMBER 10 //linefeed
+#define CR_NUMBER 13 // /t
+#define MAX_OUTPUT_NUMBER 10 //output max to 10
+
 #include<iostream>
 #include<string.h>
 #include<fstream> 
@@ -7,3 +13,8 @@
 string InputFile(const char* filename);
 
 int CountChar(string inputString);
+
+bool cmp(pair<string, int> a, pair<string, int> b);
+bool IsBlankChar(char c);
+bool NotNumberOrLetter(char s);
+bool IsEnglishLetter(char s);
