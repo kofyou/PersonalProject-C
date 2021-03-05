1bdf75c36262b41e9606bb4a26dfdc10f873d009
yangzishen <76687060+yangzishen@users.noreply.github.com>
2021-03-05 16:11:40 +0800

    InputFile
    更新输入模块的函数体

--- 221801126/src/Lib.cpp
+++ 221801126/src/Lib.cpp
@@ -1,1 +1,12 @@
+#include "lib.h"
 
+using namespace std;
+
+//InputFileMode
+string InputFile(const char* filename){
+	  ifstream in(filename, ios::in);
+		istreambuf_iterator<char> beg(in), end;
+		string sstring(beg, end);	
+		in.close();		
+		return sstring;
+}
