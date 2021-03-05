314e1a2af163aa3193f5386953e884e1c97f2c86
lidaming1 <65325238+lidaming1@users.noreply.github.com>
2021-03-05 15:27:14 +0800

    Update FileIo.h

--- 221801332/src/FileIo.h
+++ 221801332/src/FileIo.h
@@ -11,7 +11,7 @@ using namespace std;
 class FileIO
 {
 public:
-	static bool ReadChar(int argc, char *argv[], string &CharBuf,vector<string> &LinesBuf);
+	static bool ReadChar(int argc, char *argv[], string &CharBuf, vector<string> &LinesBuf);
 	static void OutputToFile(int argc, char* argv[], int CharacterCount, int LineCount, int WordCount, vector<map<string,int>::iterator> &Top10Word);
 private:
 	static string GetInputFileName(int argc, char *argv[]);
