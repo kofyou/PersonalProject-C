ebd6a9b58fb12518b0c4caf37921b6f4c2a566a6
NoMornings <80037319+NoMornings@users.noreply.github.com>
2021-03-05 16:28:42 +0800

    添加 统计文件的单词总数 功能

--- 221801235/src/WordCount.cpp
+++ 221801235/src/WordCount.cpp
@@ -18,6 +18,12 @@ int countChar(FILE *fp,char *argv);
 //判断是否为ASCII字符
 int isascii(char ch);
 
+//统计文件的单词总数 
+int countWord(FILE *fp,char *argv);
+
+//判断是否为单词
+int isWord(int cntAlpha,int cntNum,int increase);
+
 //统计文件的有效行数
 int countRow(FILE *fp,char *argv);
 
@@ -31,6 +37,10 @@ int main(int argc,char *argv[]) {
 	
 	//统计文件的字符数
 	countChar(fp,argv[2]);
+	rewind(fp);
+	
+	//统计文件的单词总数 
+	countWord(fp,argv[2]);
 	rewind(fp);
 	
 	//统计文件的有效行数 
@@ -67,6 +77,49 @@ int countChar(FILE *fp,char *argv){
 int isascii(char ch){
 	return(ch>=0 && ch<=127);
 } 
+
+//统计文件的单词总数
+int countWord(FILE *fp,char *argv){
+	char ch; 
+	//单词总数、字母总数、数字总数 
+	int cntWord,cntAlpha,cntNum; 
+	cntWord=cntAlpha=cntNum=0;
+	
+	//标志cntWord是否已经加一 
+	int increase=0;
+	
+	while((ch=fgetc(fp))!=EOF){ 
+		if(isalnum(ch)){
+			if(isdigit(ch)){
+				cntNum++;
+				//出现数字，则将cntAlpha清零 
+				cntAlpha=0;
+			}
+			else if(isalpha(ch)){
+				cntAlpha++;
+				if(isWord(cntAlpha,cntNum,increase)) {
+					cntWord++;
+					increase=1; 
+				}
+			}
+		}
+		//遇到非字母数字的字符，将cntNum,cntAlpha清零 
+		else{
+			cntNum=cntAlpha=increase=0;
+		}
+	}
+	//统计文件的单词总数 
+	ofstream output(argv, ios::app);
+	output<<"单词总数为"<<cntWord<<std::endl;
+//	printf("单词总数为%d\n",cntWord);
+	return cntWord;
+}
+
+//判断是否为单词
+int isWord(int cntAlpha,int cntNum,int increase){
+	//字母连续出现4次,前面没有数字且此单词尚未计入总数 
+	return(cntAlpha>=4 && cntNum==0 && increase==0);
+}
 
 //统计文件的有效行数
 //任何包含非空白字符的行，都需要统计
