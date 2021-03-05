b7011ce39c1dadcfe1c98ea0f51bab5b5010a203
yangzishen <76687060+yangzishen@users.noreply.github.com>
2021-03-05 17:35:09 +0800

    Update Lib.h
    更新输出函数 添加常量

--- 221801126/src/Lib.h
+++ 221801126/src/Lib.h
@@ -1,16 +1,22 @@
 #pragma  once
 
 #define POINTER_DISTANCE_NUMBER 4 //pointer distance between 1to2 is four
-#define BLANK_SPACE_NUMBER 32 //blankspace
+#define BLANK_SPACE_NUMBER 32 //blankspace ' '
 #define LINE_FEED_NUMBER 10 //linefeed
-#define CR_NUMBER 13 // /t
+#define T_NUMBER 9 // \t
+#define CR_NUMBER 13 //enter \r
 #define MAX_OUTPUT_NUMBER 10 //output max to 10
 
 #include<iostream>
 #include<string.h>
 #include<fstream> 
+#include<map> 
+#include<vector>
+#include<algorithm>
+using namespace std;
 
 string InputFile(const char* filename);
+void OutputFlie(char* filename,string inputString) ;
 
 int CountChar(string inputString);
 int CountWord(string inputString);
